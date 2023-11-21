920332f19096fecc1050ef1996285b973e72148b
saeedafra <saeed.afrasiabi@gmail.com>
2023-11-21 13:38:19 +0100

    minor polish

--- assignment_2/example2_1.cpp
+++ assignment_2/example2_1.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main() {
-    
     int n;
     cout << "How many stars?";
     cin >> n;
 
-    for(int i=0;i<n; i++)
+    for(int i=0; i<n; i++)
         cout << "*";
     cout << endl;
 
--- assignment_2/example2_2.cpp
+++ assignment_2/example2_2.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-    
     int n_col, n_row;
+    
     cout << "number of rows?";
     cin >> n_row;
     cout << "number of columns?";
     cin >> n_col;
 
-    for(int i=0;i<n_row; i++)
+    for(int i=0; i<n_row; i++)
     {
-        for(int j=0;j<n_col;j++)
+        for(int j=0; j<n_col; j++)
             cout << "*";
         cout << endl;
     }   
