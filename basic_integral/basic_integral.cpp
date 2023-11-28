11953f7f29f0cb78dd04551101709cd24f6ff80d
alirje <ali.rajaei@mail.um.ac.ir>
2023-11-28 17:00:39 +0330

    comments added / library changed to cmath

--- basic_integral/basic_integral.cpp
+++ basic_integral/basic_integral.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-    cout << "Iterations ?";
-    int n;
-    cin >> n;
-    int N=n;
-    double delta ,sum;
 
-    for(int i=0; i<n ;i++){
+    double delta ,sum;
+    int n,N;
+
+    cout << "Iterations ?";
+    cin >> n;
+    
+    N=n; //Upper limit of sigma
+
+
+    for(int i=0; i<n ;i++){ // Loop iterate n times 
         sum=0;
         delta=2 * M_PI/N;
-        for(int j=0 ; j<N;j++){
-            sum+=sin(delta *j + delta/2)*delta;
-        }
+
+        for(int j=0 ; j<N;j++) sum+=sin(delta *j + delta/2)*delta;
+        
         
         cout << "The integral for N =" << N << " is "<< sum << endl;
-        N=N*2;
         
+        N=N*2;
     }
 }
